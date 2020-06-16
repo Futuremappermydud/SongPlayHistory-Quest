5c9afcdf70cc39a8e609cf6bace0177afc37d553
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2020-06-15 21:21:09 -0400

    add stuff idk

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "../extern/beatsaber-hook/shared/utils/il2cpp-utils.hpp"
 #include "../extern/beatsaber-hook/shared/utils/utils.h"
 #include "../extern/beatsaber-hook/include/modloader.hpp"
+#include "../extern/BeatSaberQuestCustomUI-master/shared/customui.hpp"
 
 #include <math.h>
 
@@ -13,10 +14,27 @@ using namespace il2cpp_utils;
 
 static const Logger* logger;
 
+Il2CppObject* _playCount;
+
 MAKE_HOOK_OFFSETLESS(StandardLevelDetailView_RefreshContent, void, Il2CppObject* self) {
     StandardLevelDetailView_RefreshContent(self);
     bool ShowPlayerStats = *GetFieldValue<bool>(self, "_showPlayerStats");
     if(!ShowPlayerStats) return;
+    Il2CppObject* Level = *GetFieldValue(self, "_level");
+    Il2CppString* LevelID = *GetPropertyValue<Il2CppString*>(Level, "levelID");
+
+    Il2CppObject* SelectedBeatmapDifficulty = *GetFieldValue(self, "_selectedDifficultyBeatmap");
+    int Difficulty = *GetPropertyValue<int>(SelectedBeatmapDifficulty, "difficulty");
+    Il2CppObject* beatMapData = *GetPropertyValue(SelectedBeatmapDifficulty, "beatmapData");
+    Il2CppObject* parentDifficultyBeatmapSet = *GetPropertyValue(SelectedBeatmapDifficulty, "parentDifficultyBeatmapSet");
+    Il2CppObject* beatmapCharacteristic = *GetPropertyValue(parentDifficultyBeatmapSet, "beatmapCharacteristic");
+
+    Il2CppObject* PlayerData = *GetFieldValue(self, "_playerData");
+    Il2CppObject* playerLevelStatsData = *RunMethod(PlayerData, "GetPlayerLevelStatsData", LevelID, Difficulty, beatmapCharacteristic);
+    int Plays = *GetPropertyValue<int>(playerLevelStatsData, "playCount");
+    //StandardLevelDetail_Transform
+    Il2CppObject* StandardLevelDetailView_Transform = *GetPropertyValue(self, "transform");
+    
 }
 
 extern "C" void setup(ModInfo& info) {
